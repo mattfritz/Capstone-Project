12ce71de5ef361216f1ce1740302a56c93f09eca
Matt Fritz <mfritzer@gmail.com>
2016-05-19 23:53:29 -0600

    Play with Butterknife onclick bindings

--- app/src/main/java/com/example/mfritz/resethabits/RoutineActivity.java
+++ app/src/main/java/com/example/mfritz/resethabits/RoutineActivity.java
@@ -1,16 +1,33 @@
 package com.example.mfritz.resethabits;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RoutineActivity extends AppCompatActivity {
     private final String LOG_TAG = this.getClass().getSimpleName();
+
+    @BindView(R.id.fab) FloatingActionButton fab;
+
+    @OnClick(R.id.fab)
+    public void testListener(View view) {
+        Snackbar.make(view, "Check this out!", Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
