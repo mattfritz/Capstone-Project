086f0e58519988142eb71937acec9f4d0ec7d0f0
Matt Fritz <mfritzer@gmail.com>
2016-05-19 22:46:41 -0600

    Add stub fragment to main Routine activity

---
+++ app/src/main/java/com/example/mfritz/resethabits/RoutineFragment.java
@@ -0,0 +1,24 @@
+package com.example.mfritz.resethabits;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RoutineFragment extends Fragment {
+    private final String LOG_TAG = this.getClass().getSimpleName();
+
+    public RoutineFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_routine, container, false);
+    }
+}
